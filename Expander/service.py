ad1a59424223b75f8ad345302cefdcd89ba77f91
Boyue Li <me@boyue.li>
2018-03-26 18:28:20 -0400

    Disable useless expanders

--- Expander/service.py
+++ Expander/service.py
@@ -2,8 +2,8 @@ import logging
 from logging import config
 
 from Expander import NoneExpander
-from SnomedctExpander import SnomedctExpander
-from UMLSExpander import UMLSExpander
+#from SnomedctExpander import SnomedctExpander
+#from UMLSExpander import UMLSExpander
 
 logging.config.fileConfig('logging.ini')
 
@@ -11,11 +11,12 @@ if __name__ == "__main__":
     # Launch the tasks
     logger = logging.getLogger('main')
     tasks = []
-    for cls in (NoneExpander, SnomedctExpander, UMLSExpander):
-        instance = cls()
-        tasks.append(instance)
-        logger.info('Staring service %s', cls.__name__)
-        instance.start()
+    #for cls in (NoneExpander, SnomedctExpander, UMLSExpander):
+    cls = NoneExpander
+    instance = cls()
+    tasks.append(instance)
+    logger.info('Staring service %s', cls.__name__)
+    instance.start()
 
     # And wait for them to terminate
     logger.info('Waiting for the tasks to end.')
